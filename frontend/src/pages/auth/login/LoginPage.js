64dd0106bb4edd10afa8e510af82377af611de68
Srinivasssaravanan <137177741+Srinivasssaravanan@users.noreply.github.com>
2025-05-10 00:53:35 +0530

    Update LoginPage.js

--- frontend/src/pages/auth/login/LoginPage.js
+++ frontend/src/pages/auth/login/LoginPage.js
@@ -25,11 +25,11 @@ const LoginPage = () => {
 					method: "POST",
 					credentials: "include",
 					headers: {
-						"Content-type": "application/json",// json format la anupren to server.. Tells server you're sending JSON
-						Accept: "application/json",//"error": "Invalid username or password"|| “I want the response in JSON format.”||Tells server you want JSON back
+						"Content-type": "application/json",
+						Accept: "application/json",
 					},
-					body: JSON.stringify({ username, password }),//if i type name it is in js normal object format and "Content-type": "application/json" this says it(server) accepts only json format and then  Accept: "application/json" this says i want response in json format but when i type name and password in login form it is in js normal object format.... so that the typed username and password should be converted into json format right so that body: JSON.stringify({ username, password }), is used 
-				});// na anupura data is stored in body
+					body: JSON.stringify({ username, password }),
+				});
 				const data = await res.json();
 				if (!res.ok) {
 					throw new Error(data.error || "Something went wrong");
@@ -41,9 +41,8 @@ const LoginPage = () => {
 		onSuccess: () => {
 			toast.success("Logged in successfully");
 			//refetch the authuser
-			queryCLient.invalidateQueries({ //if i logged in then the data is stored in cache after sometime the cache may get stale right so while at login at another time the data must be changed so that the cache will be changed to fresh instead of old stale login
+			queryCLient.invalidateQueries({ 
 				queryKey: ["authUser"]
-        //oho ok if user enters login credentials and submit then if both are correct then it makes toast message and refetch the authUser so that it updates the data of the authUser for that particulaar user like browser cache tme ans fresh tme and all these stuffs so that while refetching using key authUser it runs the authUser and the data of that particular user is sent to the data and it is retuned so that authUser contains some data so it moves to home pag
 			});
 		},
 	});
@@ -59,10 +58,9 @@ const LoginPage = () => {
 
 	return (
 		<div className="max-w-screen-xl mx-auto flex h-screen"> 
-		{/* On large screens and above (lg: breakpoint in Tailwind), this overrides hidden and displays the div as a flex container.
-            So, this section only appears on large screens (typically ≥1024px width).*/}
-			<div className="flex-1 hidden lg:flex items-center justify-center">{/**usually at first it is hidden but after lg:flex it acts as a flex container */}
-				<XSvg className="lg:w-2/3 fill-white" /> {/**large,hidden for small  */}
+		
+			<div className="flex-1 hidden lg:flex items-center justify-center">
+				<XSvg className="lg:w-2/3 fill-white" /> 
 			</div>
 			<div className="flex-1 flex flex-col justify-center items-center">
 				<form className="flex gap-5 flex-col" onSubmit={handleSubmit}>
@@ -124,7 +122,3 @@ const LoginPage = () => {
 };
 
 export default LoginPage;
-/**
- two logos or xsvg one for large another for small
- if img size is large then image comes left of text if xsvg is small then it comes to top of text
- */
